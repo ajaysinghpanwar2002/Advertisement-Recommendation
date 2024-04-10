91001095bde6b3fe5cdf93d3fa4530d7b92aa8cc
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2024-04-10 06:52:22 +0530

    class is getting generated on the recomendation side

--- src/services/recommendation/controllers/recommendationController.ts
+++ src/services/recommendation/controllers/recommendationController.ts
@@ -1,9 +1,33 @@
 import { Request, Response } from 'express';
 import * as tf from '@tensorflow/tfjs-node';
 import { User } from '../models/users';
+import { faker } from '@faker-js/faker';
 
 class recommendationController {
     private model = tf.sequential();
+    private isModelTrained = false;
+    private isDemoUserDatabasePopulated = false;
+
+    constructor() {
+        this.populateDemoUserDatabase();
+        this.trainModelOnce();
+    }
+
+    private async populateDemoUserDatabase() {
+        if (!this.isDemoUserDatabasePopulated) {
+            await this.insertDemoUserDatabase();
+            this.isDemoUserDatabasePopulated = true;
+        }
+    }
+
+    private async trainModelOnce() {
+        if (!this.isModelTrained) {
+            this.createModel();
+            const { X, Y } = this.createTrainingData();
+            await this.trainModel(X, Y);
+            this.isModelTrained = true;
+        }
+    }
 
     // Create the model
     private createModel() {
@@ -17,23 +41,26 @@ class recommendationController {
 
     // Create training data
     private createTrainingData() {
-        const sampleData = [
-            { age: 25, gender: 'Male', embeddingVector: [0.6, 0.4] },
-            { age: 35, gender: 'Female', embeddingVector: [0.3, 0.7] },
-            { age: 18, gender: 'Male', embeddingVector: [0.7, 0.3] },
-            { age: 42, gender: 'Female', embeddingVector: [0.4, 0.6] },
-            { age: 27, gender: 'Male', embeddingVector: [0.8, 0.2] },
-            { age: 22, gender: 'Female', embeddingVector: [0.5, 0.5] },
-            { age: 31, gender: 'Male', embeddingVector: [0.7, 0.3] },
-            { age: 28, gender: 'Female', embeddingVector: [0.3, 0.7] },
-            { age: 19, gender: 'Male', embeddingVector: [0.6, 0.4] },
-            { age: 39, gender: 'Female', embeddingVector: [0.4, 0.6] },
-        ];
-        const X = sampleData.map(({ age, gender }) => {
+        //const sampleTrainingData = [
+        //    { age: 25, gender: 'Male', embeddingVector: [0.6, 0.4] },
+        //    { age: 35, gender: 'Female', embeddingVector: [0.3, 0.7] },
+        //    { age: 18, gender: 'Male', embeddingVector: [0.7, 0.3] },
+        //    { age: 42, gender: 'Female', embeddingVector: [0.4, 0.6] },
+        //    { age: 27, gender: 'Male', embeddingVector: [0.8, 0.2] },
+        //    { age: 22, gender: 'Female', embeddingVector: [0.5, 0.5] },
+        //    { age: 31, gender: 'Male', embeddingVector: [0.7, 0.3] },
+        //    { age: 28, gender: 'Female', embeddingVector: [0.3, 0.7] },
+        //    { age: 19, gender: 'Male', embeddingVector: [0.6, 0.4] },
+        //    { age: 39, gender: 'Female', embeddingVector: [0.4, 0.6] },
+        //];
+
+        const sampleTrainingData = this.generateSyntheticUserDataModelTraining(50);
+
+        const X = sampleTrainingData.map(({ age, gender }) => {
             const genderOneHot = gender === 'Male' ? [1, 0] : [0, 1];
             return [age, ...genderOneHot];
         });
-        const Y = sampleData.map(({ embeddingVector }) => embeddingVector);
+        const Y = sampleTrainingData.map(({ embeddingVector }) => embeddingVector);
         return { X, Y };
     }
 
@@ -49,7 +76,8 @@ class recommendationController {
     }
 
     private getUserEmbedding(age: any, gender: any) {
-        const userTensor = tf.tensor2d([[age, gender]]);
+        const genderOneHot = gender === 'Male' ? [1, 0] : [0, 1];
+        const userTensor = tf.tensor2d([[age, ...genderOneHot]]);
         const prediction = this.model.predict(userTensor);
         if (Array.isArray(prediction)) {
             return prediction.map(tensor => tensor.dataSync());
@@ -73,9 +101,9 @@ class recommendationController {
     public async recommendationProvider(req: Request, res: Response) {
         try {
             const { age, gender } = req.body;
-            this.createModel();
-            const { X, Y } = this.createTrainingData();
-            await this.trainModel(X, Y);
+            // this.createModel();
+            // const { X, Y } = this.createTrainingData();
+            // await this.trainModel(X, Y);
             const recommendedClass = await this.getRecommendedClass(age, gender);
 
             res.status(200).json(recommendedClass);
@@ -129,8 +157,106 @@ class recommendationController {
                 advertisementClassCounts[a] > advertisementClassCounts[b] ? a : b
             );
         }
-        console.log("recommended class",recommendedClass);
+        console.log("recommended class", recommendedClass);
         return { advertisement_class: recommendedClass };
+    }
+
+    private async insertDemoUserDatabase() {
+        const sampleUserData = [
+            {
+                age: 25,
+                gender: 'Male',
+                advertisement_interactions: [
+                    { advertisement_class: 'Technology', time_duration: 120 },
+                    { advertisement_class: 'Shopping', time_duration: 90 },
+                ],
+            },
+            {
+                age: 35,
+                gender: 'Female',
+                advertisement_interactions: [
+                    { advertisement_class: 'Entertainment', time_duration: 180 },
+                    { advertisement_class: 'Travel', time_duration: 150 },
+                ],
+            },
+            {
+                age: 18,
+                gender: 'Male',
+                advertisement_interactions: [
+                    { advertisement_class: 'Technology', time_duration: 60 },
+                    { advertisement_class: 'Entertainment', time_duration: 45 },
+                ],
+            },
+            {
+                age: 42,
+                gender: 'Female',
+                advertisement_interactions: [
+                    { advertisement_class: 'Health and Wellness', time_duration: 90 },
+                    { advertisement_class: 'Travel', time_duration: 120 },
+                ],
+            },
+            {
+                age: 27,
+                gender: 'Male',
+                advertisement_interactions: [
+                    { advertisement_class: 'Automotive', time_duration: 75 },
+                    { advertisement_class: 'Shopping', time_duration: 60 },
+                ],
+            },
+            {
+                age: 22,
+                gender: 'Female',
+                advertisement_interactions: [
+                    { advertisement_class: 'Entertainment', time_duration: 120 },
+                    { advertisement_class: 'Food and Beverage', time_duration: 90 },
+                ],
+            },
+            {
+                age: 31,
+                gender: 'Male',
+                advertisement_interactions: [
+                    { advertisement_class: 'Financial Services', time_duration: 105 },
+                    { advertisement_class: 'Home and Real Estate', time_duration: 75 },
+                ],
+            },
+            {
+                age: 28,
+                gender: 'Female',
+                advertisement_interactions: [
+                    { advertisement_class: 'Travel', time_duration: 135 },
+                    { advertisement_class: 'Services', time_duration: 90 },
+                ],
+            },
+            {
+                age: 19,
+                gender: 'Male',
+                advertisement_interactions: [
+                    { advertisement_class: 'Education', time_duration: 60 },
+                    { advertisement_class: 'Non-Profit and Social Causes', time_duration: 45 },
+                ],
+            },
+            {
+                age: 39,
+                gender: 'Female',
+                advertisement_interactions: [
+                    { advertisement_class: 'Health and Wellness', time_duration: 120 },
+                    { advertisement_class: 'Financial Services', time_duration: 90 },
+                ],
+            },
+        ];
+
+        await User.insertMany(sampleUserData);
+    }
+
+    private generateSyntheticUserDataModelTraining(count: number) {
+        const data = [];
+        for (let i = 0; i < count; i++) {
+            const age = faker.number.int({ min: 18, max: 65 });
+            const gender = faker.person.gender();
+            const embeddingVector = [faker.number.int({ min: 0, max: 1 }), faker.number.int({ min: 0, max: 1 })];
+            data.push({ age, gender, embeddingVector });
+        }
+        return data;
     }
 }
 
